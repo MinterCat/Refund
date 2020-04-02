4eb7611779a1de9b2a5363500ded2ec25f22db9d
MinterCat <admin@mintercat.com>
2020-04-02 19:09:32 +0500

    MinterCat | Refund

---
+++ nodes.php
@@ -0,0 +1,33 @@
+<?php
+$array_node = array(
+	array(
+	'from' => 'bipmaker',
+	'node_key' => 'Mpaaaa175f44e40e31d0f16fc05fc65e5802dcdf40fe897dd762076d633bc1aaaa',
+	'name' => 'BipMakerPRO node',
+	'comm' => 6
+	),
+	array(
+	'from' => 'en',
+	'node_key' => 'Mp999d3789d40ff0c699f861758bcafde15d3b4828c7518bc94810837688888888',
+	'name' => 'Enlightenment node',
+	'comm' => 7
+	),
+	array(
+	'from' => 'musk',
+	'node_key' => 'Mp7373d6d5a4ec18d87d766e5b4c6d3a0c94b357c4460a9ed377bf5d09fcd77373',
+	'name' => 'Elon Musk node',
+	'comm' => 3
+	),
+	array(
+	'from' => 'unode',
+	'node_key' => 'Mp02bc3c3f77d5ab9732ef9fc3801a6d72dc18f88328c14dc735648abfe551f50f',
+	'name' => 'U-node',
+	'comm' => 3
+	),
+	array(
+	'from' => 'zen',
+	'node_key' => 'Mp03478aae43a1a660573fab0763ae44492cdaf8deffc3fcbcc844acd67dfb2db6',
+	'name' => 'Zen node',
+	'comm' => 3
+	)
+);
